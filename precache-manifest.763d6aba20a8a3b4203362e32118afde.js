73d8b3008e9ab3bc9c5c2114d33140127a4f9fd1
howardng940990575 <hhng@ucsc.edu>
2019-11-24 17:53:48 -0800

    Updates

---
+++ precache-manifest.763d6aba20a8a3b4203362e32118afde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b22842a1c2c8814798342a89e508103",
+    "url": "/cse184_final_project_railroad_travel/index.html"
+  },
+  {
+    "revision": "b55b328d7efd7f3150f4",
+    "url": "/cse184_final_project_railroad_travel/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "451dd6f63ef203ac85e5",
+    "url": "/cse184_final_project_railroad_travel/static/js/2.08e6acaf.chunk.js"
+  },
+  {
+    "revision": "b55b328d7efd7f3150f4",
+    "url": "/cse184_final_project_railroad_travel/static/js/main.f5bb1334.chunk.js"
+  },
+  {
+    "revision": "7ef32084ae7467cd3cd9",
+    "url": "/cse184_final_project_railroad_travel/static/js/runtime-main.e669a6c7.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/cse184_final_project_railroad_travel/static/media/logo.25bf045c.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cse184_final_project_railroad_travel/precache-manifest.763d6aba20a8a3b4203362e32118afde.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cse184_final_project_railroad_travel/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
