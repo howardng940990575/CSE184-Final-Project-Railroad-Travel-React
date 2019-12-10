b07dd346dbb1c51de1ecafd9d5df1075dabe4bfc
howardng940990575 <hhng@ucsc.edu>
2019-12-09 17:50:57 -0800

    Updates

--- precache-manifest.2a00064bd61f03c76e3161dc7b7a9464.js
+++ precache-manifest.2a00064bd61f03c76e3161dc7b7a9464.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aee3a8438d044839bcc5909c26b49bc2",
+    "revision": "4ecd9bfe03b8175d969e9c06df07b096",
     "url": "/CSE184-Final-Project-Railroad-Travel-React/index.html"
   },
   {
-    "revision": "313f33f407b9f381ff7d",
-    "url": "/CSE184-Final-Project-Railroad-Travel-React/static/css/main.498d002a.chunk.css"
+    "revision": "1a4205a4816fdd5ab822",
+    "url": "/CSE184-Final-Project-Railroad-Travel-React/static/css/main.34de6062.chunk.css"
   },
   {
     "revision": "73f180a48b417573e040",
     "url": "/CSE184-Final-Project-Railroad-Travel-React/static/js/2.76fdbcf2.chunk.js"
   },
   {
-    "revision": "313f33f407b9f381ff7d",
-    "url": "/CSE184-Final-Project-Railroad-Travel-React/static/js/main.22a78862.chunk.js"
+    "revision": "1a4205a4816fdd5ab822",
+    "url": "/CSE184-Final-Project-Railroad-Travel-React/static/js/main.323a976f.chunk.js"
   },
   {
     "revision": "b7bb09247eb79c89bd21",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/CSE184-Final-Project-Railroad-Travel-React/precache-manifest.6a9011da88b78a3866038761b10819e1.js"
+  "/CSE184-Final-Project-Railroad-Travel-React/precache-manifest.2a00064bd61f03c76e3161dc7b7a9464.js"
 );
 
 self.addEventListener('message', (event) => {
